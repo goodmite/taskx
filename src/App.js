18f0ac9765a4d059d5a91fc33eddedf5476063ea
good mite <sandeepkgupta007@gmail.com>
2021-03-26 15:52:16 +0530

    fix: done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
-import * as d3 from "d3";
-
-const width = 500;
-const height = 500;
-const centerX = width / 2;
-const centerY = height / 2;
-const strokeWidth = 10;
-const eyeRadius = 50;
-const eyeOffsetX = width / 6;
-const eyeOffsetY = 50;
-
-const mouthWidth = 20;
-const mouthRadius = 140;
-
-const mouthArc = d3.arc()
-    .innerRadius(mouthRadius)
-    .outerRadius(mouthRadius + mouthWidth)
-    .startAngle(Math.PI* 2.5 / 4)
-    .endAngle(Math.PI * 5.5 / 4);
 
 function App() {
+    const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
+    const handleMouseMove = e => {
+        setMousePosition({x: e.clientX, y: e.clientY});
+    }
     return (
-        <div className="App">
-            <svg width={width} height={height}>
-                <g transform={`translate(${centerX},${centerY})`}>
-                    <circle cx={0} r={centerY - strokeWidth} stroke={'black'}
-                            strokeWidth={strokeWidth}
-                            fill={'yellow'}/>
-                    <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius}/>
-                    <circle cx={eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius}/>
-                    <path d={mouthArc()}/>
-                </g>
+        <div className="App" onMouseMove={_ => handleMouseMove(_)}>
+            <svg width={300} height={300}>
+                <circle fill={'yellow'} r={10} cy={mousePosition.y} cx={mousePosition.x}/>
             </svg>
         </div>
     );
