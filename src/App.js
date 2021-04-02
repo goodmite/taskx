4be0cc6770f8252843b51d653b2f4aa4c8ada06f
good mite <sandeepkgupta007@gmail.com>
2021-04-02 16:26:55 +0530

    bar-char done

--- src/App.js
+++ src/App.js
@@ -3,36 +3,86 @@ import './App.css';
 import * as d3 from "d3";
 import {useState, useEffect} from "react";
 
-const url = "https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv";
+const url =
+    'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv';
 
-const height = window.innerHeight;
-const width = window.innerWidth;
-const arc = d3.arc()
-    .innerRadius(0)
-    .outerRadius(height*100)
+const height = 500;
+const width = 800;
+const margin = {
+    top: 20,
+    bottom: 60,
+    left: 220,
+    right: 10,
+}
+
 function App() {
-    let message;
     const [data, setData] = useState(null);
     useEffect(_ => {
-        d3.csv(url)
+        const row = d => {
+            /*create another row named 2020, which contains int, not string*/
+            d.population = (+d['2020']) * 1000;
+            return d;
+        }
+        d3.csv(url, row)
             .then(dataArg => {
-                setData(dataArg);
+                setData(dataArg.slice(0, 10));
             })
     }, [])
+    if (!data) {
+        return <pre>Loading...</pre>;
+    }
+
+    const innerHeight = height - margin.top - margin.bottom;
+    const innerWidth = width - margin.left - margin.right;
+    const axisLabelOffset = 60;
+    const yScale = d3.scaleBand()
+        .domain(data.map(d => d.Country))
+        .range([0, innerHeight])
+        .paddingInner(0.1);
+    const maxPop = d3.max(data, d => d.population);
+    const xScale = d3.scaleLinear()
+        .domain([0, maxPop])
+        .range([0, innerWidth])
     return (
-        <svg width={width} height={height} overflow={'hidden'}>
-            <g transform={`translate(${width/10}, ${height/5})`}>
-                {
-                    data && data.map((e, index) => {
-                        return (
-                            <path height={200} fill={e['RGB hex value']} d={arc({
-                                startAngle:(index * 2* Math.PI/(data.length)),
-                                endAngle: ((index + 1) * 2* Math.PI/(data.length)),
-                            })}></path>
-                        )
-                    })}
-            </g>
-        </svg>
+        <>
+            <svg width={width} height={height}>
+                <g transform={`translate(${margin.left}, ${margin.right})`}>
+                    {
+                        xScale.ticks().map(tickValue => (
+                            <g className={'tick'} transform={`translate(${xScale(tickValue)}, ${0})`}>
+                                <line y2={innerHeight} stroke={'black'}/>
+                                <text style={{'textAnchor': 'middle'}} y={innerHeight + 5} dy=".71em">
+                                    {d3.format(".2s")(tickValue).replace('G', 'B')}
+                                </text>
+                            </g>
+                        ))
+                    }
+                    {
+                        yScale.domain().map(tickValue => (
+                            <g className={'tick'} transform={`translate(${0}, ${yScale(tickValue) + yScale.bandwidth()/2})`}>
+                                <text style={{'textAnchor': 'end'}} x={-3} dy={'.32em'}> {tickValue} </text>
+                            </g>
+                        ))
+                    }
+                    <text className={'axis-label'} x={innerWidth/2} y={innerHeight + axisLabelOffset} textAnchor={'middle'}>Population</text>
+                    {data.map((d) => (
+                        <rect
+                            className={'mark'}
+                            key={d.Country}
+                            y={yScale(d.Country)}
+                            width={xScale(d.population)}
+                            height={yScale.bandwidth()}
+                        >
+                            <title>
+                                {
+                                    d3.format(".2s")((d.population)).replace('G', 'B')
+                                }
+                            </title>
+                        </rect>
+                    ))}
+                </g>
+            </svg>
+        </>
     );
 }
 
